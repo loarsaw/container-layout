264c4a9d03d8773ac16a8ca5f2adefd29204d029
loarsaw <amanahmed1385@gmail.com>
2022-05-29 15:08:06 +0530

    final

--- src/Info.js
+++ src/Info.js
@@ -1,6 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Info = () => {
-  return <div>Info</div>;
-};
+function Info() {
+  const [user, setUser] = useState({});
+
+  const getUser = async () => {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    const person = data.results[0];
+    const { phone, email } = person;
+    const { large: image } = person.picture;
+    const { first, last } = person.name;
+    const newPerson = {
+      image,
+      phone,
+      email,
+      name: `${first} ${last}`,
+    };
+    setUser(newPerson);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return (
+    <div class="box">
+      <div class="card">
+        <div class="imgBx">
+          <img
+            src={user.image}
+            alt="images"
+          />
+        </div>
+        <div class="details">
+          <h2>
+           {user.name}
+            <br />
+            <span>{user.email}</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default Info;
